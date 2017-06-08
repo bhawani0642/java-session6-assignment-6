6380a333ae70f1b28d8be0a51f642b5c942cb44a
bhawani0642 <bhawani0642@gmail.com>
2017-06-08 14:33:10 +0530

    public class
    public class

---
+++ src/assignment6/ClassA.java
@@ -0,0 +1,34 @@
+package assignment6;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i love java");
+	}
+
+}
+
+class ClassB {
+	public static void main(String[] args) {
+		System.out.println("I am learning java programming");
+	}
+}
+
+class ClassC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I will also learn android");
+	}
+
+}
+
+class ClassD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Android is also intresting ");
+	}
+
+}
